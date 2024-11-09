7900761a4d402dd2b95ebc1d777b3c9aed204aac
Vanessa Castro <vanafac@hotmail.com>
2024-11-08 22:59:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 
-const porta = 3002;
-const host = '0.0.0.0';
+const porta = 3000;
+const host = 'localhost';
 
 var listaProdutos = [];
 
@@ -62,23 +62,26 @@ function cadastroProdutoView (req, resp){
 
 function menuView (req, resp){
     resp.send(`
-        <html>
-                <head>
-                    <title>Cadastro de produtos</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                    
-                </head>
-                <body>
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">MENU</a>
-                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div class="navbar-nav">
-                                    <a class="nav-link active" aria-current="page" href="/cadastrarProduto">Cadastrar Produto</a>
-                                </div
-                            </div>
-                        </div>
-                    </nav>
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cadastro de Cliente</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            </head>
+            <body>
+                  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                     <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Menu Principal</a>
+                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                     <span class="navbar-toggler-icon"></span>
+                 </button>
+                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                 <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/cadastrarProduto">Cadastrar Produto</a>
+                 </li>
                 </body>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </html>     
